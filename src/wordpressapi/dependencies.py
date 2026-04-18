25156f0271fe35b42bce080429bacea09b5954ec
Forever Park <140984288+foreverpark@users.noreply.github.com>
2026-04-17 20:17:49 -0600

    Include orders endpoint

--- src/wordpressapi/dependencies.py
+++ src/wordpressapi/dependencies.py
@@ -3,8 +3,8 @@ from woocommerce import API
 # Configuración de la conexión
 wcapi = API(
     url="http://localhost:8080", # Asegúrate de que el puerto sea el correcto (8080)
-    consumer_key="ck_f6731ec8696f0c00fdb54c5b929f3bf671ea1f16",
-    consumer_secret="cs_50f271c2a00faaf9d37787d54a5713ea18a0faca",
+    consumer_key="ck_b94f8db11dba9585630408e11415e45a7fd843f9",
+    consumer_secret="cs_2d5c790adfaf1160a7ea7633101f5360ffbee6b5",
     version="wc/v3",
     timeout=20
 )
