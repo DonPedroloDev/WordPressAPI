b568abfadd751e34be76485330e745cc98999066
Forever Park <140984288+foreverpark@users.noreply.github.com>
2026-04-17 20:18:36 -0600

    Reset keys lol

--- src/wordpressapi/dependencies.py
+++ src/wordpressapi/dependencies.py
@@ -3,8 +3,8 @@ from woocommerce import API
 # Configuración de la conexión
 wcapi = API(
     url="http://localhost:8080", # Asegúrate de que el puerto sea el correcto (8080)
-    consumer_key="ck_b94f8db11dba9585630408e11415e45a7fd843f9",
-    consumer_secret="cs_2d5c790adfaf1160a7ea7633101f5360ffbee6b5",
+    consumer_key="ck_X",
+    consumer_secret="cs_X",
     version="wc/v3",
     timeout=20
 )
